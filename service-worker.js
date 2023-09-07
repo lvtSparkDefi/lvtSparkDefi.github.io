32fb6d1e5c01deb345506cd556956fcfc9d8bb81
lvtSparkDefi <>
2023-09-07 10:48:34 +0800

    update page

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ffe43efe44f261641a91df5cfdd7ecc7.js"
+  "/precache-manifest.5520d4dce8d7928831067265451bbf29.js"
 );
 
 self.addEventListener('message', (event) => {
